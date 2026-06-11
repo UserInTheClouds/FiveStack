4a81a5f1f1a778e1230f57e18a845638185e6c7b
HarshJ <harshjoshi561234@gmail.com>
2026-06-11 12:43:40 +0530

    google analytics added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState,useEffect } from 'react'
-import {Navigate, Route,Routes} from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import zustandStore from './misc/zustand.utility.js'
 import Login from './pages/loginPage.jsx'
@@ -7,6 +7,9 @@ import Signup from './pages/signupPage.jsx'
 import Chat from './pages/ChatPage.jsx'
 import Landing from './pages/LandingPage.jsx'
 import ForgotPassword from './pages/forgotPasswordPage.jsx';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-SJ7NDM2L7H");
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,27 +17,27 @@ function App() {
   const { authUser, isCheckingAuth, checkAuth, connectSocket, disconnectSocket } = zustandStore();
 
   useEffect(() => {
-    checkAuth(); 
+    checkAuth();
   }, []);
 
-  useEffect(()=>{
-    if(authUser){
+  useEffect(() => {
+    if (authUser) {
       connectSocket();
     }
-    else{
+    else {
       disconnectSocket();
     }
-  },[authUser,connectSocket,disconnectSocket]);
+  }, [authUser, connectSocket, disconnectSocket]);
 
-   if (isCheckingAuth) return <div className='bg-black w-screen h-screen text-white flex items-center justify-center text-4xl' >Loading...</div>;
+  if (isCheckingAuth) return <div className='bg-black w-screen h-screen text-white flex items-center justify-center text-4xl' >Loading...</div>;
 
   return (
     <Routes>
-        <Route path='/' element={!authUser?<Landing/>:<Navigate to='/chat'/>} />
-        <Route path='/chat' element={authUser?<Chat/>:<Navigate to='/'/>} />
-        <Route path='/login' element={!authUser?<Login/>:<Navigate to='/chat'/>} />
-        <Route path='/signup' element={!authUser?<Signup/>:<Navigate to='/chat'/>} />
-        <Route path='/forgot-password' element={!authUser ? <ForgotPassword/> : <Navigate to='/chat'/>} />
+      <Route path='/' element={!authUser ? <Landing /> : <Navigate to='/chat' />} />
+      <Route path='/chat' element={authUser ? <Chat /> : <Navigate to='/' />} />
+      <Route path='/login' element={!authUser ? <Login /> : <Navigate to='/chat' />} />
+      <Route path='/signup' element={!authUser ? <Signup /> : <Navigate to='/chat' />} />
+      <Route path='/forgot-password' element={!authUser ? <ForgotPassword /> : <Navigate to='/chat' />} />
     </Routes>
   )
 }
